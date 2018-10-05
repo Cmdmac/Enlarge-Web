8d04af050b0d182a2d54b48a3cbfc0a834650a0e
cmdmac <cmdmac@126.com>
2018-10-05 17:49:19 +0800

    add tree, table

--- src/components/FileManager.vue
+++ src/components/FileManager.vue
@@ -1,18 +1,48 @@
 <template>
-    <div class="content">
-
-        <div class="left">
-            <el-tree
-                    :props="props1"
-                    :load="loadNode1"
-                    lazy
-                    show-checkbox>
-            </el-tree>
+    <div>
+        <div class="header">
+            <input placeholder="搜索"/>
+            <button>列表</button>
+            <button>网格</button>
         </div>
-        <div class="right">
-            kdkksdfs
+        <div class="navigator">
+            <button>Test</button>
         </div>
-
+        <div class="content">
+            <div class="left">
+                <el-tree
+                        :props="tree_items"
+                        :load="loadNode"
+                        :accordion = "true"
+                        @node-click="onNodeClick"
+                        :expand-on-click-node="false"
+                        lazy>
+                     <span class="custom-tree-node" slot-scope="{ node, data }">
+                          <span> <i v-if="node.data.isDir" class="el-icon-info"></i>{{ node.label }}</span>
+                     </span>
+                </el-tree>
+            </div>
+            <div class="right">
+                <el-table
+                        :data="tableData"
+                        style="width: 100%; height: 100%;">
+                    <el-table-column
+                            prop="date"
+                            label="日期"
+                            width="180">
+                    </el-table-column>
+                    <el-table-column
+                            prop="name"
+                            label="姓名"
+                            width="180">
+                    </el-table-column>
+                    <el-table-column
+                            prop="address"
+                            label="地址">
+                    </el-table-column>
+                </el-table>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -24,30 +54,84 @@
         },
         data() {
             return {
-                props1: {
+                tree_items: {
                     label: 'name',
                     children: 'zones',
                     isLeaf: 'leaf'
                 },
+                files: [
+                    {name: "360", isDir: true, icon: "el-icon-info" }, {name: "tencent", isDir: true }, {name: "xiaomi", isDir: true, leaf: true}, {name: "test.txt", isDir: false, leaf: true}, {name: "中文.doc", isDir: false, leaf: true}
+                ],
+                tableData: []
             };
         },
         methods: {
-            loadNode1(node, resolve) {
-                if (node.level === 0) {
-                    return resolve([{ name: 'region' }]);
-                }
-                if (node.level > 1) return resolve([]);
-
+            getFiles(dir, resolve) {
                 setTimeout(() => {
-                    const data = [{
-                        name: 'leaf',
-                        leaf: true
-                    }, {
-                        name: 'zone'
-                    }];
-
+                    const data = this.files;
                     resolve(data);
                 }, 500);
+            },
+
+            loadNode(node, resolve) {
+                if (node.level === 0) {
+                    return resolve([{name: 'region', isDir: true, path: "/sss/ddd"}]);
+                }
+//                if (node.level > 1) return resolve([]);
+                if (!node.isLeaf) {
+                    this.getFiles(node.data.path, resolve);
+                }
+            },
+
+            onNodeClick(data, node) {
+                if (node.childNodes.length <= 0 && !node.isloded && !node.isloading) {
+                    //load data
+//                    node.data = this.files;
+                    //build path
+                    var path = node.data.name;
+                    var tmp = node.parent;
+                    while (tmp != null && tmp.data != undefined) {
+                        path = tmp.data.name + '/' + path;
+                        tmp = tmp.parent;
+                    }
+                    //eslint-disable-next-line
+                    console.log(path);
+
+                    node.loadData();
+                }
+                //eslint-disable-next-line
+                console.log(data);
+                //eslint-disable-next-line
+                console.log(node);
+                var files = [];
+                var nodes = node.childNodes;
+                /*
+                {
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1517 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1516 弄'
+                }
+                 */
+                for(let i =  0; i < nodes.length; i++) {
+                    //eslint-disable-next-line
+//                    console.log(nodes[i].data);
+//                    files.push(nodes[i].data);
+                    this.tableData.push({date: '2018-10-04', name: nodes[i].data.name, address: 'hhaha'});
+                }
+                //eslint-disable-next-line
+                console.log(files);
             }
         }
     }
@@ -56,16 +140,33 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
+    .header {
+        width: 100%;
+        display: flex;
+    }
+
+    .navigator {
+        width: 100%;
+        display: flex;
+    }
+
     .content {
         display: flex;
         width: 100%;
+        padding-bottom: 5px;
     }
 
     .left {
         width: 30%;
+        height: 100%;
+        border-width: 1px;
+        border-color: lightgrey;
+        border-style: solid;
     }
 
     .right {
         width: 100%;
+        padding-bottom: 5px;
+        border: solid 1px lightgrey;
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import '../theme/index.css'
 
 import Vue from 'vue'
 import App from './App.vue'
-import { Button, Tree } from 'element-ui';
+import { Button, Tree, Table, TableColumn } from 'element-ui';
 
 Vue.config.productionTip = false
 
 Vue.use(Button);
 Vue.use(Tree);
+Vue.use(Table);
+Vue.use(TableColumn);
 
 new Vue({
   render: h => h(App)
